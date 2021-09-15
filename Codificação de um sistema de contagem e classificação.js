66c91afe202c05e7dfacdd55b92133a4f7df7017
Pedrocarvalho2001 <89496667+Pedrocarvalho2001@users.noreply.github.com>
2021-09-14 21:01:57 -0300

    Add files via upload

---
+++ Codificação de um sistema de contagem e classificação.js
@@ -0,0 +1,25 @@
+
+ var numero = 0
+ var total = 15
+
+ for(var contador = 0;contador<=total;contador++ ){
+    if(contador == 0){
+        console.log('O numero é zero');
+    }else{
+        if(contador%2 == 0){
+            console.log('O numero ' + contador + ' é par');
+        }else{
+            if(contador%2 == 1){
+                console.log('O numero ' + contador + ' é impar');
+            }else{
+                
+            }
+
+
+
+        }
+    }
+}          
+
+
+
